715d12cdc0c052dbdbc311b3d9e601aadd5beef7
akhilbinoy <akhilbinoy@akhilbinoys-MacBook-Air.local>
2026-05-11 16:11:14 +0530

    only hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,9 @@
-import { EvilCursor } from '@/components/EvilCursor'
-import { AgentDetectionRing } from '@/components/AgentDetectionRing'
-import { Navbar } from '@/components/Navbar'
 import { Hero } from '@/components/Hero'
-import { Problem } from '@/components/Problem'
-import { Solution } from '@/components/Solution'
-import { HowItWorks } from '@/components/HowItWorks'
-import { Features } from '@/components/Features'
-import { TechnicalDiff } from '@/components/TechnicalDiff'
-import { Demo } from '@/components/Demo'
-import { CTA } from '@/components/CTA'
-import { Footer } from '@/components/Footer'
 
 export default function Home() {
   return (
-    <>
-      <EvilCursor />
-      <AgentDetectionRing />
-      <Navbar />
-      <main>
-        <Hero />
-        <Problem />
-        <Solution />
-        <HowItWorks />
-        <Features />
-        <TechnicalDiff />
-        <Demo />
-        <CTA />
-        <Footer />
-      </main>
-    </>
+    <main>
+      <Hero />
+    </main>
   )
 }
